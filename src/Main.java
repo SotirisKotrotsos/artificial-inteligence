3f4ddcaa5699fac2811714da53272fe80b3fce47
Sotiris Kotrotsos <sotiriskotrotsos@hotmail.com>
2021-11-27 17:43:51 +0200

    Final

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,6 @@ public class Main {
         State initialState = new State(numOfPeople, spaceOfBoat, maxNumOfRoutes);
         SpaceSearcher searcher = new SpaceSearcher(initialState);
         long start = System.currentTimeMillis();
-        //State terminalState = searcher.AStar(initialState);
         State terminalState = searcher.BestFSClosedSet(initialState);
         long end = System.currentTimeMillis();
         if(terminalState == null){
@@ -25,20 +24,19 @@ public class Main {
             State temp = terminalState;
             ArrayList<State> path = new ArrayList<>();
             path.add(terminalState);
-            while(temp.getFather() != null) // if father is null, then we are at the root.
+            while(temp.getFather() != null)
             {
                 path.add(temp.getFather());
                 temp = temp.getFather();
             }
-            // reverse the path and print.
+
             Collections.reverse(path);
             for(State item: path)
             {
-                //System.out.println(item.toString());
                 item.print();
             }
             System.out.println();
-            System.out.println("Search time:" + (double)(end - start) / 1000 + " sec.");  // total time of searching in seconds.
+            System.out.println("Search time:" + (double)(end - start) / 1000 + " sec.");
 
 
         }
